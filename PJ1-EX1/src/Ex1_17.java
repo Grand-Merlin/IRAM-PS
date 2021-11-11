e11860464f63fd7b429ace2e38e5b074272c6309
François <Franois@192.168.1.42>
2021-11-11 14:05:19 +0100

    test push 2

--- PJ1-EX1/src/Ex1_17.java
+++ PJ1-EX1/src/Ex1_17.java
@@ -25,7 +25,7 @@ public class Ex1_17 {
 				return choice;
 			}//fin si
 
-		}while (choice.contains(expectedAnswers) == false);//test push
+		}while (choice.contains(expectedAnswers) == false);//test push 2
 	}//fin getUserSpecificInput
 
 
